1fff1cd53e02a3613154b5d11edfe1c16a631fe2
baiying.zhu <baiying.zhu@qunar.com>
2014-01-20 14:59:11 +0800

    update

--- dist/js/ZUI.js
+++ dist/js/ZUI.js
@@ -1,4 +1,4 @@
-/*! ZUI - v0.0.1 - 2014-01-17
+/*! ZUI - v0.0.1 - 2014-01-20
 * https://github.com/zhiyan/ZUI
 * Copyright (c) 2014 zhiyan; Licensed MIT */
 // init
@@ -30,12 +30,23 @@ zui.directive("ngMenu",function($http){
 
 	function link(scope, element, attrs){
 		var url = attrs['url'];
+		scope.open = false;
 		$http.get(url).success(function(data) {
 		    scope.menus = data.data.menus;
-		 });
+		});
+		scope.showMenu = function(target){
+			scope.open = scope.open ? false : true;
+			if(scope.open) {
+				$(target).nextAll(".menu-c").hide();
+				$(target).find(".glyphicon").removeClass("glyphicon-minus");
+			} else {
+				$(target).nextAll(".menu-c").show()
+				$(target).find(".glyphicon").addClass("glyphicon-minus");
+			}
+		}
 	}
 
 	 return {
       link:link
     };
-})
+});
