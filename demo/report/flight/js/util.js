2e814389d024852158ec1cac4335b6818541f859
baiying.zhu <baiying.zhu@qunar.com>
2014-01-21 18:58:44 +0800

    update

--- demo/report/flight/js/util.js
+++ demo/report/flight/js/util.js
@@ -1,35 +1,35 @@
 // service
 angular.module('zuiServices', []);
-zui.directive("ngSearch",function(){
-	function searchBox(scope, element, attrs){
-		scope.box = $("#search-opation");
-		scope.icon = $(".glyphicon");
-		scope.box.find("tr:gt(2)").not(":last-child").hide();
-		scope.flag = false;
-		scope.showMore = function(target){
-			if(scope.flag){
-				scope.box.find("tr:gt(2)").not(":last-child").hide();
-				scope.icon.removeClass("glyphicon-minus");
-				scope.flag = false;
-			} else {
-				scope.box.find("tr").show();
-				scope.icon.addClass("glyphicon-minus");
-				scope.flag = true;
-			}
-			
-		}
-	}
+zui.directive("ngSearch", function() {
+    function searchBox(scope, element, attrs) {
+        scope.box = $("#search-opation");
+        scope.icon = $(".glyphicon");
+        scope.box.find("tr:gt(2)").not(":last-child").hide();
+        scope.flag = false;
+        scope.showMore = function(target) {
+            if (scope.flag) {
+                scope.box.find("tr:gt(2)").not(":last-child").hide();
+                scope.icon.removeClass("glyphicon-minus");
+                scope.flag = false;
+            } else {
+                scope.box.find("tr").show();
+                scope.icon.addClass("glyphicon-minus");
+                scope.flag = true;
+            }
 
-	return {
-      link:searchBox
+        }
+    }
+
+    return {
+        link: searchBox
     };
 });
-zui.directive("ngChart",function(){
-	function columnChart(scope, element, attrs){
-  		var colors = Highcharts.getOptions().colors,
-        categories = ['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera'],
-        name = 'Browser brands',
-        data = [{
+zui.directive("ngChart", function() {
+    function columnChart(scope, element, attrs) {
+        var colors = Highcharts.getOptions().colors,
+            categories = ['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera'],
+            name = 'Browser brands',
+            data = [{
                 y: 55.11,
                 color: colors[0],
                 drilldown: {
@@ -53,7 +53,8 @@ zui.directive("ngChart",function(){
                 drilldown: {
                     name: 'Chrome versions',
                     categories: ['Chrome 5.0', 'Chrome 6.0', 'Chrome 7.0', 'Chrome 8.0', 'Chrome 9.0',
-                        'Chrome 10.0', 'Chrome 11.0', 'Chrome 12.0'],
+                        'Chrome 10.0', 'Chrome 11.0', 'Chrome 12.0'
+                    ],
                     data: [0.12, 0.19, 0.12, 0.36, 0.32, 9.91, 0.50, 0.22],
                     color: colors[2]
                 }
@@ -63,7 +64,8 @@ zui.directive("ngChart",function(){
                 drilldown: {
                     name: 'Safari versions',
                     categories: ['Safari 5.0', 'Safari 4.0', 'Safari Win 5.0', 'Safari 4.1', 'Safari/Maxthon',
-                        'Safari 3.1', 'Safari 4.1'],
+                        'Safari 3.1', 'Safari 4.1'
+                    ],
                     data: [4.55, 1.42, 0.23, 0.21, 0.20, 0.19, 0.14],
                     color: colors[3]
                 }
@@ -73,91 +75,91 @@ zui.directive("ngChart",function(){
                 drilldown: {
                     name: 'Opera versions',
                     categories: ['Opera 9.x', 'Opera 10.x', 'Opera 11.x'],
-                    data: [ 0.12, 0.37, 1.65],
+                    data: [0.12, 0.37, 1.65],
                     color: colors[4]
                 }
             }];
 
-    	function setChart(name, categories, data, color) {
-			chart.xAxis[0].setCategories(categories, false);
-			chart.series[0].remove(false);
-			chart.addSeries({
-				name: name,
-				data: data,
-				color: color || 'white'
-			}, false);
-			chart.redraw();
-    	}
-    	var chart = $('#container').highcharts({
-	        chart: {
-	            type: 'column'
-	        },
-	        title: {
-	            text: 'Browser market share, April, 2011'
-	        },
-	        subtitle: {
-	            text: 'Click the columns to view versions. Click again to view brands.'
-	        },
-	        xAxis: {
-	            categories: categories
-	        },
-	        yAxis: {
-	            title: {
-	                text: ''
-	            }
-	        },
-	        plotOptions: {
-	            column: {
-	                cursor: 'pointer',
-	                point: {
-	                    events: {
-	                        click: function() {
-	                            var drilldown = this.drilldown;
-	                            if (drilldown) { // drill down
-	                                setChart(drilldown.name, drilldown.categories, drilldown.data, drilldown.color);
-	                            } else { // restore
-	                                setChart(name, categories, data);
-	                            }
-	                        }
-	                    }
-	                },
-	                dataLabels: {
-	                    enabled: true,
-	                    color: colors[0],
-	                    style: {
-	                        fontWeight: 'bold'
-	                    },
-	                    formatter: function() {
-	                        return this.y +'%';
-	                    }
-	                }
-	            }
-	        },
-	        tooltip: {
-	            formatter: function() {
-	                var point = this.point,
-	                    s = this.x +':<b>'+ this.y +'% market share</b><br/>';
-	                if (point.drilldown) {
-	                    s += 'Click to view '+ point.category +' versions';
-	                } else {
-	                    s += 'Click to return to browser brands';
-	                }
-	                return s;
-	            }
-	        },
-	        series: [{
-	            name: name,
-	            data: data,
-	            color: 'white'
-	        }],
-	        exporting: {
-	            enabled: false
-	        }
-	    })
-	    .highcharts(); // return chart			
-	}
+        function setChart(name, categories, data, color) {
+            chart.xAxis[0].setCategories(categories, false);
+            chart.series[0].remove(false);
+            chart.addSeries({
+                name: name,
+                data: data,
+                color: color || 'white'
+            }, false);
+            chart.redraw();
+        }
+        var chart = $('#container').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Browser market share, April, 2011'
+            },
+            subtitle: {
+                text: 'Click the columns to view versions. Click again to view brands.'
+            },
+            xAxis: {
+                categories: categories
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                }
+            },
+            plotOptions: {
+                column: {
+                    cursor: 'pointer',
+                    point: {
+                        events: {
+                            click: function() {
+                                var drilldown = this.drilldown;
+                                if (drilldown) { // drill down
+                                    setChart(drilldown.name, drilldown.categories, drilldown.data, drilldown.color);
+                                } else { // restore
+                                    setChart(name, categories, data);
+                                }
+                            }
+                        }
+                    },
+                    dataLabels: {
+                        enabled: true,
+                        color: colors[0],
+                        style: {
+                            fontWeight: 'bold'
+                        },
+                        formatter: function() {
+                            return this.y + '%';
+                        }
+                    }
+                }
+            },
+            tooltip: {
+                formatter: function() {
+                    var point = this.point,
+                        s = this.x + ':<b>' + this.y + '% market share</b><br/>';
+                    if (point.drilldown) {
+                        s += 'Click to view ' + point.category + ' versions';
+                    } else {
+                        s += 'Click to return to browser brands';
+                    }
+                    return s;
+                }
+            },
+            series: [{
+                name: name,
+                data: data,
+                color: 'white'
+            }],
+            exporting: {
+                enabled: false
+            }
+        })
+            .highcharts(); // return chart			
+    }
 
-	return {
-      link:columnChart
+    return {
+        link: columnChart
     };
 });
--- src/directive/menu.js
+++ src/directive/menu.js
@@ -1,28 +1,28 @@
-zui.directive("ngMenu",function($http){
+zui.directive("ngMenu", function($http) {
 
-	function link(scope, element, attrs){
-		var url = attrs['url'];
-		$http.get(url).success(function(data) {
-		    scope.menus = data.data.menus;
-		});
-		scope.showMenu = function(target){
-			if($(target).attr("flag") === "true") {
-				$(target).nextAll(".menu-c").hide();
-				$(target).find(".glyphicon").removeClass("glyphicon-minus");
-				$(target).attr("flag", false);
-			} else {
-				$(target).nextAll(".menu-c").show()
-				$(target).find(".glyphicon").addClass("glyphicon-minus");
-				$(target).attr("flag", true);
-			}
-		}
-		scope.menuBg = function(target){
-			$(target).addClass("menu-active");
-			$(target).siblings().removeClass("menu-active");
-		}
-	}
+    function link(scope, element, attrs) {
+        var url = attrs['url'];
+        $http.get(url).success(function(data) {
+            scope.menus = data.data.menus;
+        });
+        scope.showMenu = function(target) {
+            if ($(target).attr("flag") === "true") {
+                $(target).nextAll(".menu-c").hide();
+                $(target).find(".glyphicon").removeClass("glyphicon-minus");
+                $(target).attr("flag", false);
+            } else {
+                $(target).nextAll(".menu-c").show()
+                $(target).find(".glyphicon").addClass("glyphicon-minus");
+                $(target).attr("flag", true);
+            }
+        }
+        scope.menuBg = function(target) {
+            $(target).addClass("menu-active");
+            $(target).siblings().removeClass("menu-active");
+        }
+    }
 
-	 return {
-      link:link
+    return {
+        link: link
     };
 });
